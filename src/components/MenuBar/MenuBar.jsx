ca52abd939eaf230a418be3cde17864867766bd8
Prajit <prajitkaushik21b@gmail.com>
2025-11-08 23:05:08 +0530

    updated navbar

---
+++ src/components/MenuBar/MenuBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import logo from '../../assets/logo.jpg';
+import cart from '../../assets/cart.png'
+const MenuBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container">
+                <img src={logo} alt="" height={65} width={75} />
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse mx-3" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Explore</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" href='#'>Contact Us</a>
+                        </li>
+                    </ul>
+
+                    <div className="menubar-right d-flex align-items-center gap-3">
+                        <div className="position-relative">
+                            <img src={cart} width={40} height={40} alt="" className="position-relative" />
+                            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning">5</span>
+                        </div>
+                        <button type="button" className="btn btn-danger ms-4">Log In</button>
+                        <button type="button" className="btn btn-outline-danger">Register</button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default MenuBar
