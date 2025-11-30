431b10349f2583c58cd1f2ad56b6363346a22c34
Prajit <prajitkaushik21b@gmail.com>
2025-11-30 22:35:08 +0530

    deployment updates

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import MenuBar from '../../components/MenuBar/Menubar'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
